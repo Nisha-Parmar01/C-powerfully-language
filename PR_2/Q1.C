fab093ef79fd65f697996d70bca31a0dd536c52a
Nisha-Parmar01 <nishaparmar.0901@gmail.com>
2024-08-13 10:30:20 +0530

    13/08/2024

---
+++ PR_2/Q1.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+     int n;
+     clrscr();
+     printf("Enter the marks:");
+     scanf("%d",&n);
+
+
+
+     (n>91  &&   n<=100)
+	    ?printf("your grade is A")
+	    :
+	      (n>81 && n<90)
+	      ?printf("your grade is B")
+	      :
+		(n>71 && n<=80)
+		?printf("your garde is C")
+		:
+		  (n>61 && n<=70)
+		  ?printf("your garde is D")
+		  :
+		    (n>51 && n<=60)
+		     ?printf("your grade is E")
+		     :
+		      (n>34 && n<=50)
+		       ?printf("your garde is f")
+		       :printf("sorry you are fail");
+
+
+
+
+     getch();
+}
---
+++ PR_2/Q2.C
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+     int n;
+     char h;
+     clrscr();
+     printf("Enter the marks:");
+     scanf("%d",&n);
+
+
+
+     if(n>=91  &&   n<=100)
+     {
+	h = 'A';
+     }
+     else if(n>=81 && n<=90)
+     {
+	h = 'B';
+     }
+     else if(n>=71  && n<=80)
+     {
+	h = 'C';
+     }
+     else if (n>=61  && n<=70)
+     {
+	h =  'D';
+     }
+     else if (n>=51   && n<=60)
+     {
+	h = 'E';
+     }
+     else
+     {
+       h = 'F';
+     }
+
+	    switch(h)
+	    {
+		case 'A':
+		    printf("Excellent work!");
+		    break;
+
+		case 'B':
+		    printf("Well done");
+		    break;
+		case 'C':
+		    printf("Good job");
+		    break;
+		case 'D':
+		    printf("You passed,but you could do better");
+		    break;
+		case 'E':
+		    printf("you are passed, but better try next time.");
+		    break;
+		case 'F':
+		    printf(" 'sorry you are fail' ");
+		    break;
+		default :
+		  printf("invallid...");
+	     }
+     getch();
+}
+
+
+
+
+
+
+
+
+
+
